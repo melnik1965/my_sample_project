ce23945473a5a2896b0bfc8acd568387c23e95df
melnik1965 <melnik1965@yandex.ru>
2026-03-29 11:53:55 +0300

    Настройки runner

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 
 print(os.uname_result)
 print(sys.executable)
-print("Изменение в веб браузере")
+print("Изменения в веб браузере")
 print("Изменение в веб браузере 1")
 print("После перезагрузки от ПК")
-print("После перезагрузки от github")
+print("После перезагрузки от github") 
